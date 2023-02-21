43ea39f60bcb8dd236ca29495b53be45c02e88e2
diegg22unimet <dmanzanilla@correo.unimet.edu.ve>
2023-02-21 16:29:07 -0400

    Building ManageStorage

--- app/src/main/java/organized/chaos/Database.java
+++ app/src/main/java/organized/chaos/Database.java
@@ -9,12 +9,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Andres
  */
 public class Database {
+
     private static final String STORES_SECTION = "Almacenes;";
     private static final String ROUTES_SECTION = "Rutas;";
     private static final String STORE_PREFIX = "Almacen ";
@@ -23,67 +25,64 @@ public class Database {
     private static final int READING_STOCK = 2;
     private static final int READING_ROUTE = 3;
 
-    private Database() {}
-    
+    private Database() {
+    }
+
     public static StoreGraph readDatabase(File file) throws IOException {
         return readDatabase(new FileReader(file));
     }
-    
+
     public static StoreGraph readDatabase(Reader baseReader) throws IOException {
         StoreGraph graph = new StoreGraph();
-        
-        try(BufferedReader reader = new BufferedReader(baseReader)) {
-            
+
+        try (BufferedReader reader = new BufferedReader(baseReader)) {
+
             int mode = READING_NULL;
             Store store = null;
 
-            for(String line = reader.readLine(); line != null; line = reader.readLine()) {
-                if(line.startsWith(STORES_SECTION)) {
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                if (line.startsWith(STORES_SECTION)) {
                     mode = READING_STORE;
-                }
-                else if(line.startsWith(ROUTES_SECTION)) {
+                } else if (line.startsWith(ROUTES_SECTION)) {
                     mode = READING_ROUTE;
-                }
-                else if(mode == READING_STORE) {
+                } else if (mode == READING_STORE) {
                     String name = line.replace(":", "").replace(STORE_PREFIX, "");
                     store = graph.createStore(name);
                     mode = READING_STOCK;
-                }
-                else if(mode == READING_STOCK) {
+                } else if (mode == READING_STOCK) {
                     String[] data = line.replace(";", "").split(",");
-                    if(store != null && data.length >= 2) {
+                    if (store != null && data.length >= 2) {
                         Stock stock = new Stock(data[0], Integer.parseInt(data[1]));
                         store.getStock().append(stock);
                     }
-                    if(line.endsWith(";")) {
+                    if (line.endsWith(";")) {
                         mode = READING_STORE;
                     }
-                }
-                else if(mode == READING_ROUTE) {
+                } else if (mode == READING_ROUTE) {
                     String[] data = line.split(",");
-                    if(data.length >= 3) {
+                    if (data.length >= 3) {
                         graph.createRoute(data[0], data[1], Integer.parseInt(data[2]));
                     }
                 }
             }
         }
-        
+
         return graph;
     }
-    
+
     public static void writeDatabase(StoreGraph graph, File file) throws IOException {
         writeDatabase(graph, new FileWriter(file));
     }
-    
+
     public static void writeDatabase(StoreGraph graph, Writer baseWriter) throws IOException {
-        try(BufferedWriter writer = new BufferedWriter(baseWriter)) {
-            
+        try (BufferedWriter writer = new BufferedWriter(baseWriter)) {
+
             writer.write("%s\n".formatted(STORES_SECTION));
-            
-            for(ListNode<Store> storeNode = graph.getStores().getFirst(); storeNode != null; storeNode = storeNode.getNext()) {
+
+            for (ListNode<Store> storeNode = graph.getStores().getFirst(); storeNode != null; storeNode = storeNode.getNext()) {
                 Store store = storeNode.getValue();
                 writer.write("%s%s:".formatted(STORE_PREFIX, store.getName()));
-                for(ListNode<Stock> stockNode = store.getStock().getFirst(); stockNode != null; stockNode = stockNode.getNext()) {
+                for (ListNode<Stock> stockNode = store.getStock().getFirst(); stockNode != null; stockNode = stockNode.getNext()) {
                     Stock stock = stockNode.getValue();
                     writer.write("\n%s,%d".formatted(stock.getProduct(), stock.getAmount()));
                 }
@@ -94,12 +93,12 @@ public class Database {
             pending.append(graph.getStores().getFirst().getValue());
 
             writer.write("%s\n".formatted(ROUTES_SECTION));
-            
-            for(ListNode<Store> storeNode = pending.getFirst(); storeNode != null; storeNode = storeNode.getNext()) {
+
+            for (ListNode<Store> storeNode = pending.getFirst(); storeNode != null; storeNode = storeNode.getNext()) {
                 Store store = storeNode.getValue();
-                for(ListNode<Route> routeNode = store.getRoutes().getFirst(); routeNode != null; routeNode = routeNode.getNext()) {
+                for (ListNode<Route> routeNode = store.getRoutes().getFirst(); routeNode != null; routeNode = routeNode.getNext()) {
                     Route route = routeNode.getValue();
-                    if(visited.find(route.getStore().getName()) == null) {
+                    if (visited.find(route.getStore().getName()) == null) {
                         pending.append(route.getStore());
                         writer.write("%s,%s,%d\n".formatted(store.getName(), route.getStore().getName(), route.getDistance()));
                     }
@@ -110,5 +109,32 @@ public class Database {
 
             writer.flush();
         }
+    }
+
+    public String showStores(StoreGraph graph) {
+        String storePrint = "";
+        StoreList pending = new StoreList();
+        StoreList visited = new StoreList();
+        pending.append(graph.getStores().getFirst().getValue());
+        for (ListNode<Store> storeNode = pending.getFirst(); storeNode != null; storeNode = storeNode.getNext()) {
+            storePrint = storePrint + "Stores:\n" + storeNode.getValue().getStock() + "\n";
+        }
+        return storePrint;
+    }
+
+    public String showStock(StoreGraph graph, String selectedStore) {
+        String stockPrint = "";
+        StoreList pending = new StoreList();
+        StoreList visited = new StoreList();
+        pending.append(graph.getStores().getFirst().getValue());
+        for (ListNode<Store> storeNode = pending.getFirst(); storeNode != null; storeNode = storeNode.getNext()) {
+            Store store = storeNode.getValue();
+            if (storeNode.getValue().getName().equalsIgnoreCase(selectedStore)) {
+                for (ListNode<Stock> stockNode = store.getStock().getFirst(); stockNode != null; stockNode = stockNode.getNext()) {
+                    stockPrint = stockPrint + "- " + stockNode.getValue().getProduct() + ": " + stockNode.getValue().getAmount() + "\n";
+                }
+            }
+        }
+        return stockPrint;
     }
 }
--- app/src/main/java/organized/chaos/StoreGraph.java
+++ app/src/main/java/organized/chaos/StoreGraph.java
@@ -5,74 +5,75 @@ package organized.chaos;
  * @author Andres
  */
 public class StoreGraph {
+
     private final StoreList stores = new StoreList();
-    
+
     public Store createStore(String name) {
         Store store = new Store(name);
         stores.append(store);
         return store;
     }
-    
+
     public Store createStore(String name, Route[] routes) {
         Store store = new Store(name);
-        for(Route route : routes) {
+        for (Route route : routes) {
             store.getRoutes().append(route);
             route.getStore().getRoutes().append(new Route(store, route.getDistance()));
         }
         stores.append(store);
         return store;
     }
-    
+
     public Store removeStore(String name) {
         Store store = stores.remove(name);
-        if(store != null) {
-            for(ListNode<Store> node = stores.getFirst(); node != null; node = node.getNext()) {
+        if (store != null) {
+            for (ListNode<Store> node = stores.getFirst(); node != null; node = node.getNext()) {
                 node.getValue().getRoutes().remove(name);
             }
         }
         return store;
     }
-    
+
     public Store getStore(String name) {
         return stores.find(name);
     }
-        
+
     public void createRoute(String a, String b, int distance) {
         Store storeA = stores.find(a);
         Store storeB = stores.find(b);
-        if(storeA != null && storeB != null && storeA != storeB) {
+        if (storeA != null && storeB != null && storeA != storeB) {
             storeA.getRoutes().append(new Route(storeB, distance));
             storeB.getRoutes().append(new Route(storeA, distance));
         }
     }
-        
+
     public void removeRoute(String a, String b) {
         Store storeA = stores.find(a);
         Store storeB = stores.find(b);
-        if(storeA != null && storeB != null && storeA != storeB) {
+        if (storeA != null && storeB != null && storeA != storeB) {
             storeA.getRoutes().remove(b);
             storeB.getRoutes().remove(a);
         }
     }
-    
+
     public int getDistance(String a, String b) {
         Store storeA = stores.find(a);
-        if(storeA != null) {
+        if (storeA != null) {
             Route route = storeA.getRoutes().find(b);
-            if(route != null) {
+            if (route != null) {
                 return route.getDistance();
             }
         }
         return -1;
     }
-    
+
     public void setDistance(String a, String b, int distance) {
         Store storeA = stores.find(a);
         Store storeB = stores.find(b);
-        if(storeA != null && storeB != null && storeA != storeB) {
+        if (storeA != null && storeB != null && storeA != storeB) {
             Route routeAB = storeA.getRoutes().find(b);
             Route routeBA = storeB.getRoutes().find(a);
-            if(routeAB != null && routeBA != null) {
+            if (routeAB != null && routeBA != null) {
                 routeAB.setDistance(distance);
                 routeBA.setDistance(distance);
             }
@@ -82,4 +83,5 @@ public class StoreGraph {
     public StoreList getStores() {
         return stores;
     }
+
 }
