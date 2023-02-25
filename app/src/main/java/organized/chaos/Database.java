96ec3b3e8a4967ddda95d21c5cba3db017fe011d
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2023-02-25 00:19:19 -0400

    Cleanup

--- app/src/main/java/organized/chaos/Database.java
+++ app/src/main/java/organized/chaos/Database.java
@@ -3,13 +3,11 @@ package organized.chaos;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
-import javax.swing.JOptionPane;
 
 /**
  *
